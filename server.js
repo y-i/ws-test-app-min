3badf99e123c85a94b789f9a405198efa57fbaea
y-i <y-i@users.noreply.github.com>
2018-05-10 14:15:34 +0900

    fix variable definition

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  const now = new Date();
-  const time = `${`0${now.getHours()}`.slice(-2)}:${`0${now.getMinutes()}`.slice(-2)}:${`0${now.getSeconds()}`.slice(-2)}`
   socket.on('chat message', function(msg){
+    const now = new Date();
+    const time = `${`0${now.getHours()}`.slice(-2)}:${`0${now.getMinutes()}`.slice(-2)}:${`0${now.getSeconds()}`.slice(-2)}`
     io.emit('chat message', msg);
     io.emit('chat message', '{{NAME}}');
     io.emit('chat message', time);
